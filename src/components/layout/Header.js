c4dc66f05e3c6e562aef83f99674628fb043b064
Tarjei Utnes <tutnes@gmail.com>
2021-04-18 00:48:09 +0200

    Initial commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -115,14 +115,7 @@ const Header = ({
                       <Link to="#0" onClick={closeMenu}>Documentation</Link>
                     </li>
                   </ul>
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
-                      <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
-                      </li>
-                    </ul>}
+
                 </div>
               </nav>
             </>}
--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -1,200 +1,214 @@
 import React from 'react';
 import classNames from 'classnames';
-import { SectionTilesProps } from '../../utils/SectionProps';
+import {SectionTilesProps} from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
 import Image from '../elements/Image';
 
 const propTypes = {
-  ...SectionTilesProps.types
+    ...SectionTilesProps.types
 }
 
 const defaultProps = {
-  ...SectionTilesProps.defaults
+    ...SectionTilesProps.defaults
 }
 const FeaturesTiles = ({
-  className,
-  topOuterDivider,
-  bottomOuterDivider,
-  topDivider,
-  bottomDivider,
-  hasBgColor,
-  invertColor,
-  pushLeft,
-  ...props
-}) => {
+                           className,
+                           topOuterDivider,
+                           bottomOuterDivider,
+                           topDivider,
+                           bottomDivider,
+                           hasBgColor,
+                           invertColor,
+                           pushLeft,
+                           ...props
+                       }) => {
 
-  const outerClasses = classNames(
-    'features-tiles section',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
-    className
-  );
+    const outerClasses = classNames(
+        'features-tiles section',
+        topOuterDivider && 'has-top-divider',
+        bottomOuterDivider && 'has-bottom-divider',
+        hasBgColor && 'has-bg-color',
+        invertColor && 'invert-color',
+        className
+    );
 
-  const innerClasses = classNames(
-    'features-tiles-inner section-inner pt-0',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
-  );
+    const innerClasses = classNames(
+        'features-tiles-inner section-inner pt-0',
+        topDivider && 'has-top-divider',
+        bottomDivider && 'has-bottom-divider'
+    );
 
-  const tilesClasses = classNames(
-    'tiles-wrap center-content',
-    pushLeft && 'push-left'
-  );
+    const tilesClasses = classNames(
+        'tiles-wrap center-content',
+        pushLeft && 'push-left'
+    );
 
-  const sectionHeader = {
-    title: 'Build up the whole picture',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
-  };
+    const sectionHeader = {
+        title: 'Tutnes Holding AS',
+        paragraph: 'Formålet til selskapet er å drive med utleie av egen eller leid fast eiendom ellers. Samt investere i andre selskaper.'
+    };
 
-  return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
-      <div className="container">
-        <div className={innerClasses}>
-          <SectionHeader data={sectionHeader} className="center-content" />
-          <div className={tilesClasses}>
+    return (
+        <section
+            {...props}
+            className={outerClasses}
+        >
+            <div className="container">
+                <div className={innerClasses}>
+                    <SectionHeader data={sectionHeader} className="center-content"/>
+                    <div className={tilesClasses}>
 
-            <div className="tiles-item reveal-from-bottom">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-01.svg')}
-                      alt="Features tile icon 01"
-                      width={64}
-                      height={64} />
-                  </div>
+                        <div className="tiles-item reveal-from-bottom">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-01.svg')}
+                                            alt="Features tile icon 01"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Mano Pizza AS
+                                    </h4>
+                                    <p className="m-0 text-sm">
+                                        Mano betyr «hånd» og pizza er håndverk. Derfor lager vi den for hånd på
+                                        tradisjonelt napolitansk vis. Deigen hviler i minst ett døgn og strekkes ut for
+                                        hånd. Kombinert med vår egen pizzasaus av Toscana-tomater,gir den lette og
+                                        luftige deigen det vi synes er den perfekte pizza.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-02.svg')}
+                                            alt="Features tile icon 02"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Daal Noise Control Systems AS
+                                    </h4>
+                                    <p className="m-0 text-sm">
+
+                                        DAAL was established in 2016 in Trondheim, the technology capital of Norway.
+                                        Founders Dag Loe and Sigmund Birkeland and their team have developed a
+                                        technology that speciﬁcally targets unwanted and hazardous noise that occurs
+                                        inside a motorcycle helmet during a ride. The prototype is tested and veriﬁed
+                                        for speeds up to 140 km/h. The product is expected to be available in the market
+                                        in 2020.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-03.svg')}
+                                            alt="Features tile icon 03"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Wiral Technologies AS
+                                    </h4>
+                                    <p className="m-0 text-sm">
+                                        We’re a crowdfunded startup company - we started out with a successful Kickstarter campaign (100% funded in under 4 minutes) and have kept growing ever since.
+
+                                        Our first product - Wiral LITE, is a versatile cable cam system designed to create unique, dynamic shots. It’s an awesome accessory for film production, sports, lifestyle, coaching and other types of shoots, both indoors and outdoors.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tiles-item reveal-from-bottom">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-04.svg')}
+                                            alt="Features tile icon 04"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Robust Workflow
+                                    </h4>
+                                    <p className="m-0 text-sm">
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-05.svg')}
+                                            alt="Features tile icon 05"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Robust Workflow
+                                    </h4>
+                                    <p className="m-0 text-sm">
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+                            <div className="tiles-item-inner">
+                                <div className="features-tiles-item-header">
+                                    <div className="features-tiles-item-image mb-16">
+                                        <Image
+                                            src={require('./../../assets/images/feature-tile-icon-06.svg')}
+                                            alt="Features tile icon 06"
+                                            width={64}
+                                            height={64}/>
+                                    </div>
+                                </div>
+                                <div className="features-tiles-item-content">
+                                    <h4 className="mt-0 mb-8">
+                                        Robust Workflow
+                                    </h4>
+                                    <p className="m-0 text-sm">
+                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
             </div>
-
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-02.svg')}
-                      alt="Features tile icon 02"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-03.svg')}
-                      alt="Features tile icon 03"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiles-item reveal-from-bottom">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-04.svg')}
-                      alt="Features tile icon 04"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-05.svg')}
-                      alt="Features tile icon 05"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
-                      alt="Features tile icon 06"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 FeaturesTiles.propTypes = propTypes;
